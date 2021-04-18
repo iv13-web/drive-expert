10ce48bb60e91cb99990fdbbb9d2c73f84b2f66f
Igor <iivanovspg@gmail.com>
2021-04-18 11:59:33 +0300

    adapt

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let swiper = new Swiper('.swiper-container', {
+let racersSwiper = new Swiper('.slider-racers .swiper-container', {
     slidesPerView: 3,
     spaceBetween: 50,
     pagination: {
@@ -21,6 +21,32 @@ let swiper = new Swiper('.swiper-container', {
       }
     }
 });
+
+let mapSwiper = new Swiper('.slider-maps .swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1
+      }, 
+      1000: {
+          slidesPerView: 2
+      },
+      1200: {
+          slidesPerView: 3
+      }
+    }
+});
+
+
 
 const header = document.querySelector('.header');
 const menuLinks = document.querySelectorAll('.nav-link');
